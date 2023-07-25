db0848e378944528c1adbbc7e7be16d2b1276df6
Ahmed Ali <Ahmed.A.Qenawy@gmail.com>
2023-07-25 21:22:07 +0300

    updating cabins components

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,57 +1,35 @@
-import styled from "styled-components";
-import { useSearchParams } from "react-router-dom";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { cabins, isLoading } = useCabins();
-  const currentFilter = searchParams.get("discount");
-
-  const filteredCabins = cabins?.filter((cabin) => {
-    if (currentFilter === "all" || !currentFilter) {
-      return true;
-    }
-    if (currentFilter === "with-discount") {
-      return cabin.discount > 0;
-    }
-    if (currentFilter === "without-discount") {
-      return cabin.discount === 0;
-    }
-  });
-
-  const currentSort = searchParams?.get("sortBy")?.split("-") || [
-    "name",
-    "asc",
-  ];
-  let sortedCabins = filteredCabins?.sort((a, b) => {
-    if (currentSort[1] === "asc") {
-      return a[currentSort[0]] - b[currentSort[0]];
-    }
-    if (currentSort[1] === "desc") {
-      return b[currentSort[0]] - a[currentSort[0]];
-    }
-  });
+  const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
+
+  // 1) FILTER
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
